0d1d399ef688f13d20b1c2948fc6958ed514f20a
root <root@DESKTOP-9NKQN3B.localdomain>
2025-07-09 18:51:04 -0300

    medal~hao redimensionado

--- portifolio/medalhão/script.js
+++ portifolio/medalhão/script.js
@@ -25,7 +25,7 @@
 
   // 🌀 Inicializar o Swiper com config para PC e Mobile
   const swiper = new Swiper(".mySwiper", {
-    slidesPerView: isMobile ? 1 : 2.5,         // 1 slide no celular, 3 no PC
+    slidesPerView: isMobile ? 1 : 2.5, 
     spaceBetween: isMobile ? 10 : 30,
     loop: true,
     centeredSlides: true,

84f1ffef2f89d3d35efc9ab3b298de68e72685e4
root <root@DESKTOP-9NKQN3B.localdomain>
2025-07-30 16:57:22 -0300

    fts adicionadas

--- portifolio/WindBanners/script.js
+++ portifolio/WindBanners/script.js
@@ -1,6 +1,6 @@
 
   // 🔧 Variáveis configuráveis
-  const totalImagens = 79;
+  const totalImagens = 81;
   const pastaImagens = "imagens/";
   const nomeBase = "foto";
   const extensao = "jpg";
--- portifolio/tendaStandSemBolha/script.js
+++ portifolio/tendaStandSemBolha/script.js
@@ -1,6 +1,6 @@
 
   // 🔧 Variáveis configuráveis
-  const totalImagens = 45;
+  const totalImagens = 46;
   const pastaImagens = "imagens/";
   const nomeBase = "foto";
   const extensao = "jpg";
